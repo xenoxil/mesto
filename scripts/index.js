109cc4a157f0c2a65aba251b580c306395486fcc
Stepanov <Yuriy.Stepanov@nielsen.com>
2021-01-04 03:55:50 +0300

    сделать  кнопку удаления и форму для добавления новых элементов, унифицировать функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 //Обьявляем переменные для обработки кликов
 let profileEditBtn = document.querySelector('.profile__edit-btn');
+let popupEditProfile = document.querySelector('.popupEditProfile');
+let closeBtn = document.querySelector('.popup_closebtn');
+let closeBtnEdit = document.querySelector('.popup_closeBtnEdit');
+let closeBtnAdd = document.querySelector('.popup__closeBtnAdd');
 let popup = document.querySelector('.popup');
-let closeBtn = document.querySelector('.popup__closebtn');
+const elementsList = document.querySelector('.elements');
+const addElementBtn = document.querySelector('.profile__add-btn');
+const popupAddNewElement = document.querySelector('.popupAddNewElement')
+
+
 
 
 //обьявляем переменные для обработки инпутов
@@ -9,9 +17,13 @@ let profileName = document.querySelector('.profile__name');
 let profileTitle = document.querySelector('.profile__title');
 const inputName = document.getElementById('popup__profilename');
 const inputTitle = document.getElementById('popup__profiletitle');
+inputName.value = profileName.textContent;
+inputTitle.value = profileTitle.textContent;
+
 
 //Переменные для функции handleFormSubmit(evt)
-let formElement = document.querySelector('.popup__container');
+let formElement = document.getElementById('editProfileForm');
+let formAddNewElement = document.getElementById('addNewElementForm');
 
 //карточки "из коробки"
 const elementsTemplate = document.querySelector('#elements-template').content;
@@ -46,20 +58,20 @@ const initialCards = [{
 
 
 
-function showPopup() {
+function showPopup(popup) {
+
     popup.classList.add('popup_status_opened');
-    inputName.value = profileName.textContent;
-    inputTitle.value = profileTitle.textContent;
+
 }
 
 
-function closePopup() {
+
+
+function closePopup(popup) {
     popup.classList.remove('popup_status_opened');
 }
 
-function clickLike(event) {
-    event.target.classList.toogle('element__likebtn_active');
-}
+
 
 
 // Находим форму в DOM
@@ -69,26 +81,79 @@ function clickLike(event) {
 // она никуда отправляться не будет
 function handleFormSubmit(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.    
-    profileName.textContent = inputName.value;
-    profileTitle.textContent = inputTitle.value;
-    closePopup();
+    // Так мы можем определить свою логику отправки.
+    const target = evt.currentTarget;
+    const parent = target.closest('.popup');
+    console.log(target.closest('.popup'));
+    if (parent.classList.contains('popupEditProfile')) {
+        profileName.textContent = inputName.value;
+        profileTitle.textContent = inputTitle.value;
+    }
+    if (parent.classList.contains('popupAddNewElement')) {
+        let newElement = {};
+        newElement.link = popup__link.value;
+        newElement.name = popup__title.value;
+        addElement(newElement);
+
+    }
+
+
+    closePopup(parent);
 }
+
+
 
 function addElement(obj) {
     const userElement = elementsTemplate.cloneNode(true);
-    const elementsList = document.querySelector('.elements')
     userElement.querySelector('.element__image').src = obj.link;
     userElement.querySelector('.element__title').textContent = obj.name;
-    userElement.querySelector('.element__likebtn').addEventListener('click', clickLike);
-
-    elementsList.append(userElement);
+    elementsList.prepend(userElement);
 }
+
+
+
+
+
+
 initialCards.forEach(addElement);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
+
+
+
+
 formElement.addEventListener('submit', handleFormSubmit);
+formAddNewElement.addEventListener('submit', handleFormSubmit);
 
-profileEditBtn.addEventListener('click', showPopup);
 
-closeBtn.addEventListener('click', closePopup);
+
+
+profileEditBtn.addEventListener('click', () => {
+    showPopup(popupEditProfile);
+});
+
+addElementBtn.addEventListener('click', () => {
+    showPopup(popupAddNewElement);
+});
+
+closeBtnEdit.addEventListener('click', () => {
+    closePopup(popupEditProfile)
+});
+closeBtnAdd.addEventListener('click', () => {
+    closePopup(popupAddNewElement);
+});
+
+//делегируем лайки на родителя элементс
+elementsList.addEventListener('click', function(evt) {
+    const tar = evt.target;
+    if (tar.classList.contains('element__likebtn')) {
+        tar.classList.toggle('element__likebtn_active');
+    }
+});
+//делегируем удаление на родителя элементс
+elementsList.addEventListener('click', function(evt) {
+    const tar = evt.target;
+    if (tar.classList.contains('element__trashbtn') || tar.classList.contains('element__trashpic')) {
+        tar.closest('.element').remove();
+    }
+});
