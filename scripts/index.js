d6e405a6374460f7640f5972afce7bff05a63d9e
Xenoxil <xilus1127@gmail.com>
2021-01-06 13:57:40 +0300

    поправить обьявления переменных и разделить сабмит форм на 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,29 @@
 //Обьявляем переменные для обработки кликов
 const profileEditBtn = document.querySelector('.profile__edit-btn');
 const popupEditProfile = document.querySelector('.popupEditProfile');
-const closeBtn = document.querySelector('.popup_closebtn');
 const closeBtnEdit = document.querySelector('.popup__closeBtnEdit');
 const closeBtnAdd = document.querySelector('.popup__closeBtnAdd');
 const closeBtnImage = document.querySelector('.popup__closeBtnImage');
-const popup = document.querySelector('.popup');
+
 const elementsList = document.querySelector('.elements');
 const addElementBtn = document.querySelector('.profile__add-btn');
 const popupAddNewElement = document.querySelector('.popupAddNewElement');
 const popupImage = document.querySelector('.popupImage');
-const popup__image = document.querySelector('.popup__image');
-const popup__element = document.querySelector('.popup__element');
+const popupImagePic = document.querySelector('.popup__image');
+const popupElement = document.querySelector('.popup__element');
 
 
 
 
 //обьявляем переменные для обработки инпутов
-let profileName = document.querySelector('.profile__name');
-let profileTitle = document.querySelector('.profile__title');
+const profileName = document.querySelector('.profile__name');
+const profileTitle = document.querySelector('.profile__title');
 const inputName = document.getElementById('popup__profilename');
 const inputTitle = document.getElementById('popup__profiletitle');
 inputName.value = profileName.textContent;
 inputTitle.value = profileTitle.textContent;
+const popupLink = document.getElementById('popup__link');
+const popupTitle = document.getElementById('popup__title');
 
 
 //Переменные для функции handleFormSubmit(evt)
@@ -83,35 +84,36 @@ function closePopup(popup) {
 // Воспользуйтесь методом querySelector()
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit(evt) {
+function handleFormSubmitProfile(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
-    const target = evt.currentTarget;
-    const parent = target.closest('.popup');
-    console.log(target.closest('.popup'));
-    if (parent.classList.contains('popupEditProfile')) {
-        profileName.textContent = inputName.value;
-        profileTitle.textContent = inputTitle.value;
-    }
-    if (parent.classList.contains('popupAddNewElement')) {
-        let newElement = {};
-        newElement.link = popup__link.value;
-        newElement.name = popup__title.value;
-        addElement(newElement);
 
-    }
+    profileName.textContent = inputName.value;
+    profileTitle.textContent = inputTitle.value;
 
 
-    closePopup(parent);
+    closePopup(popupEditProfile);
 }
 
+function handleFormSubmitElement(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.    
+
+    let newElement = {};
+    newElement.link = popupLink.value;
+    newElement.name = popupTitle.value;
+    addElement(newElement);
+
+    closePopup(popupAddNewElement);
+}
 
 
 function addElement(obj) {
     const userElement = elementsTemplate.cloneNode(true);
-    userElement.querySelector('.element__image').src = obj.link;
+    const userElementImage = userElement.querySelector('.element__image');
+    userElementImage.src = obj.link;
     userElement.querySelector('.element__title').textContent = obj.name;
-    userElement.querySelector('.element__image').alt = obj.name;
+    userElementImage.alt = obj.name;
     elementsList.prepend(userElement);
 }
 
@@ -127,8 +129,8 @@ initialCards.forEach(addElement);
 
 
 
-formElement.addEventListener('submit', handleFormSubmit);
-formAddNewElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmitProfile);
+formAddNewElement.addEventListener('submit', handleFormSubmitElement);
 
 
 
@@ -164,8 +166,8 @@ elementsList.addEventListener('click', function(evt) {
 elementsList.addEventListener('click', function(evt) {
     const tar = evt.target;
     if (tar.classList.contains('element__image')) {
-        popup__image.src = tar.src;
-        popup__element.textContent = tar.alt;
+        popupImagePic.src = tar.src;
+        popupElement.textContent = tar.alt;
         showPopup(popupImage);
     }
 });
