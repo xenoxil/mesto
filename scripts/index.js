d0648a2b5ecd3757734e72ef3f5630c6e5f3b281
Xenoxil <xilus1127@gmail.com>
2021-01-09 02:03:23 +0300

    переработать функцию добавления карточек в универсальную функцию с добавлением слушателей на родительский элемент

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,13 @@ const popupEditProfile = document.querySelector('.popupEditProfile');
 const closeBtnEdit = document.querySelector('.popup__closeBtnEdit');
 const closeBtnAdd = document.querySelector('.popup__closeBtnAdd');
 const closeBtnImage = document.querySelector('.popup__closeBtnImage');
-
 const elementsList = document.querySelector('.elements');
 const addElementBtn = document.querySelector('.profile__add-btn');
 const popupAddNewElement = document.querySelector('.popupAddNewElement');
 const popupImage = document.querySelector('.popupImage');
 const popupImagePic = document.querySelector('.popup__image');
 const popupElement = document.querySelector('.popup__element');
-
-
-
-
-
-
-
+const popupList = document.querySelectorAll('.popup');
 
 //обьявляем переменные для обработки инпутов
 const profileName = document.querySelector('.profile__name');
@@ -28,7 +21,7 @@ inputName.value = profileName.textContent;
 inputTitle.value = profileTitle.textContent;
 const popupLink = document.getElementById('popup__link');
 const popupTitle = document.getElementById('popup__title');
-
+let times = 0;
 
 //Переменные для функции handleFormSubmit(evt)
 const formElement = document.getElementById('editProfileForm');
@@ -36,81 +29,37 @@ const formAddNewElement = document.getElementById('addNewElementForm');
 
 //карточки "из коробки"
 const elementsTemplate = document.querySelector('#elements-template').content;
-const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 
 
-
-
-
-
+//функция открытия попапов
 function showPopup(popup) {
     popup.classList.add('popup_status_opened');
     document.addEventListener('keydown', closeEsc);
 }
-
-
-
-
+//функция закрытия попапов
 function closePopup(popup) {
     popup.classList.remove('popup_status_opened');
     document.removeEventListener('keydown', closeEsc);
 }
 
 
-
-
-
-
-
-// Находим форму в DOM
-
-// Воспользуйтесь методом querySelector()
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//отправка формы профайла
 function handleFormSubmitProfile(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
-
     profileName.textContent = inputName.value;
     profileTitle.textContent = inputTitle.value;
-
-
     closePopup(popupEditProfile);
 }
 
+//отправка формы карточки
 function handleFormSubmitElement(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.    
-
-    let newElement = {};
+    // Так мы можем определить свою логику отправки.
+    const newElement = {};
     newElement.link = popupLink.value;
     newElement.name = popupTitle.value;
-    addElement(newElement);
-
+    assembleElement(newElement, elementsList, elementsTemplate);
     closePopup(popupAddNewElement);
 }
 
@@ -122,30 +71,69 @@ function closeEsc(evt) {
         }
     }
 };
+//функция добавления слушателя закрытия попапов по оверлею и кнопке закрыть
+function setClosePopupListener(popup) {
+    popup.addEventListener('click', (evt) => {
+        const target = evt.target;
+        if (target.classList.contains('popup') || target.classList.contains('popup__closebtn')) {
+            closePopup(popup);
+        }
+    });
+}
 
-//функция добавления карточек
-function addElement(obj) {
-    const userElement = elementsTemplate.cloneNode(true);
+//функция подготовки новой ноды
+function returnElement(obj, template) {
+    const userElement = template.cloneNode(true);
     const userElementImage = userElement.querySelector('.element__image');
     userElementImage.src = obj.link;
     userElement.querySelector('.element__title').textContent = obj.name;
     userElementImage.alt = obj.name;
-    elementsList.prepend(userElement);
+    return userElement;
+}
+//функция добавления ноды в разметку. 
+function addClone(userElement, parent) {
+    parent.prepend(userElement);
+}
+//функция сбора карточки с добавлением слушателей
+function assembleElement(obj, parent, template) {
+    addClone(returnElement(obj, template), parent);
+
+    //делегируем лайки на родителя 
+    if (times === 0) {
+        parent.addEventListener('click', function(evt) {
+            const tar = evt.target;
+            if (tar.classList.contains('element__likebtn')) {
+                tar.classList.toggle('element__likebtn_active');
+            }
+        });
+        //делегируем открытие картинок  на родителя 
+        parent.addEventListener('click', function(evt) {
+            const tar = evt.target;
+            if (tar.classList.contains('element__image')) {
+                popupImagePic.src = tar.src;
+                popupElement.textContent = tar.alt;
+                showPopup(popupImage);
+            }
+        });
+        //делегируем удаление на родителя 
+        parent.addEventListener('click', function(evt) {
+            const tar = evt.target;
+            if (tar.classList.contains('element__trashbtn') || tar.classList.contains('element__trashpic')) {
+                tar.closest('.element').remove();
+            }
+        })
+        times = 1;
+    }
 }
 
-
-
-
 //рендер карточек из коробки
-initialCards.forEach(addElement);
-
-
+initialCards.forEach((input) => {
+    assembleElement(input, elementsList, elementsTemplate);
+})
 
 //вешаем сабмит на кнопки
 formElement.addEventListener('submit', handleFormSubmitProfile);
 formAddNewElement.addEventListener('submit', handleFormSubmitElement);
-
-
 
 //открытие попапов
 profileEditBtn.addEventListener('click', () => {
@@ -156,51 +144,5 @@ addElementBtn.addEventListener('click', () => {
     showPopup(popupAddNewElement);
 });
 
-
-//закрытие попапов по оверлею и кнопке закрыть
-popupImage.addEventListener('click', (evt) => {
-    const target = evt.target;
-    if (target.classList.contains('popup') || target.classList.contains('popup__closebtn')) {
-        closePopup(popupImage);
-    }
-});
-
-popupAddNewElement.addEventListener('click', (evt) => {
-    const target = evt.target;
-    if (target.classList.contains('popup') || target.classList.contains('popup__closebtn')) {
-        closePopup(popupAddNewElement);
-    }
-});
-
-popupEditProfile.addEventListener('click', (evt) => {
-    const target = evt.target;
-    if (target.classList.contains('popup') || target.classList.contains('popup__closebtn')) {
-        closePopup(popupEditProfile);
-    }
-});
-
-
-//делегируем лайки на родителя элементс
-elementsList.addEventListener('click', function(evt) {
-    const tar = evt.target;
-    if (tar.classList.contains('element__likebtn')) {
-        tar.classList.toggle('element__likebtn_active');
-    }
-});
-
-//делегируем открытие картинок  на родителя элементс
-elementsList.addEventListener('click', function(evt) {
-    const tar = evt.target;
-    if (tar.classList.contains('element__image')) {
-        popupImagePic.src = tar.src;
-        popupElement.textContent = tar.alt;
-        showPopup(popupImage);
-    }
-});
-//делегируем удаление на родителя элементс
-elementsList.addEventListener('click', function(evt) {
-    const tar = evt.target;
-    if (tar.classList.contains('element__trashbtn') || tar.classList.contains('element__trashpic')) {
-        tar.closest('.element').remove();
-    }
-});
+//вешаем слушатели закрытия на все попапы
+popupList.forEach(setClosePopupListener);
